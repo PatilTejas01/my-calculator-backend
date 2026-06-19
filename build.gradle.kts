7634cb6a3a4dda8db07d87079dd0e7f689808fa9
tejas patil <your.tejaspatil8809@gmail.com>
2026-06-19 22:18:51 +0530

    Fix manifest attributes for cloud deployment

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,17 @@
 plugins {
-    // This tells the root project how to handle pure Kotlin backend configurations globally
-    id("org.jetbrains.kotlin.jvm") version "1.9.22" apply false
+    kotlin("jvm") version "1.9.22"
+    id("io.ktor.plugin") version "2.3.7"
+    id("com.github.johnrengelman.shadow") version "8.1.1" // Required for Fat JAR
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "com.tejas.securechatserver.ApplicationKt"
+    }
+}
+
+tasks.shadowJar {
+    manifest {
+        attributes["Main-Class"] = "com.tejas.securechatserver.ApplicationKt"
+    }
 }
